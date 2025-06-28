7db16b8ea64e1d0adf647af507886e09b995e9f0
Vinicius José <vjaab@cin.ufpe.br>
2025-06-28 16:27:55 -0300

    anime = codigo modificado para não poluir o terminal e permitir chamamento das funções e do modelo anime_original = arquivo que veio do colab hello = arquivo com as configurações do flask o resto é assets

---
+++ hello.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask import request
+from flask import url_for
+from flask import redirect
+from flask import render_template
+from anime import df_exploded, predict_score_knn
+
+
+# Inicialização do Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Visualização dos dados
+@app.route('/dashboard')
+def dashboard():
+    # Implementação futura
+    return render_template('dashboard.html')
+
+
+# Preditor de nota baseado em generos e numero de membros
+generos_lista = df_exploded['Genres'].unique().to_list()
+booleans = []
+selecionados = []
+membros = 0
+
+@app.route('/escolhageneros', methods=['GET'])
+def mostrar_generos():
+    return render_template('generos.html', generos=generos_lista)
+
+@app.route('/escolhageneros', methods=['POST'])
+def processar_generos():
+    global booleans, membros
+    global selecionados
+    selecionados = request.form.getlist('mostrar_generos')
+    booleans = [g in selecionados for g in generos_lista]
+    membros = int(request.form.get('membros', 0))
+    return redirect(url_for('mostrar_resultados'))
+
+@app.route('/resultados')
+def mostrar_resultados():
+    predicao = predict_score_knn(membros, booleans)
+    print(predicao)
+    return render_template('generos_selecionados.html', selecionados=selecionados, membros=membros, predição =predicao)
